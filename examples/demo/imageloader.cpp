e8f296963f78766c00afb6c9e4b44fff2f34c4e6
TripleWhy <TripleWhy@users.noreply.github.com>
2016-03-19 10:26:25 +0100

    Moved ImageLoader definitions to cpp

--- examples/demo/imageloader.cpp
+++ examples/demo/imageloader.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2015 Yannick Mueller
+Copyright (C) 2015-2016 Yannick Mueller
 
 This file is part of longscroll-qt.
 
@@ -19,4 +19,81 @@ along with longscroll-qt.  If not see <http://www.gnu.org/licenses/>.
 
 #include "imageloader.h"
 
+bool ImageLoader::QueueEntry::operator==(const ImageLoader::QueueEntry & other)
+{
+	return (fileName == other.fileName);
+}
+
+
 ImageLoader * ImageLoader::inst = 0;
+
+ImageLoader::ImageLoader()
+    : QThread()
+{
+}
+
+ImageLoader *ImageLoader::instance()
+{
+	if (inst == 0)
+	{
+		inst = new ImageLoader;
+		inst->start();
+	}
+	return inst;
+}
+
+void ImageLoader::addImageRequest(const QString & fileName, QObject * receiver)
+{
+	QueueEntry entry;
+	entry.fileName = fileName;
+	entry.receivers.insert(receiver);
+
+	QMutexLocker locker(&queueMutex);
+	int index = queue.indexOf(entry);
+	if (index >= 0)
+		queue[index].receivers.insert(receiver);
+	else
+		queue.enqueue(entry);
+	locker.unlock();
+}
+
+void ImageLoader::removeImageRequest(const QString & fileName, QObject * receiver)
+{
+	QueueEntry entry;
+	entry.fileName = fileName;
+
+	QMutexLocker locker(&queueMutex);
+	int index = queue.indexOf(entry);
+	if (index >= 0)
+	{
+		queue[index].receivers.remove(receiver);
+		if (queue[index].receivers.isEmpty())
+			queue.removeAt(index);
+	}
+	locker.unlock();
+}
+
+void ImageLoader::run()
+{
+	while (true)
+	{
+		QMutexLocker locker(&queueMutex);
+		if (queue.isEmpty())
+		{
+			locker.unlock();
+			msleep(100);
+			continue;
+		}
+
+		QueueEntry entry = queue.dequeue();
+		locker.unlock();
+
+		QPixmap p;
+		p.load(entry.fileName);
+		msleep(10);
+
+		for (QObject * receiver : entry.receivers)
+			emit imageLoaded(entry.fileName, receiver, p);
+		yieldCurrentThread();
+	}
+}
--- examples/demo/imageloader.h
+++ examples/demo/imageloader.h
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2015 Yannick Mueller
+Copyright (C) 2015-2016 Yannick Mueller
 
 This file is part of longscroll-qt.
 
@@ -37,10 +37,7 @@ private:
 		QString fileName;
 		QSet<QObject *> receivers;
 
-		bool operator==(QueueEntry const & other)
-		{
-			return (fileName == other.fileName);
-		}
+		bool operator==(QueueEntry const & other);
 	};
 
 	QQueue<QueueEntry> queue;
@@ -48,78 +45,16 @@ private:
 
 private:
 	static ImageLoader * inst;
-	ImageLoader()
-	    : QThread()
-	{
-	}
+	ImageLoader();
 
 public:
-	static ImageLoader * instance()
-	{
-		if (inst == 0)
-		{
-			inst = new ImageLoader;
-			inst->start();
-		}
-		return inst;
-	}
+	static ImageLoader * instance();
 
-	void addImageRequest(QString const & fileName, QObject * receiver)
-	{
-		QueueEntry entry;
-		entry.fileName = fileName;
-		entry.receivers.insert(receiver);
-
-		QMutexLocker locker(&queueMutex);
-		int index = queue.indexOf(entry);
-		if (index >= 0)
-			queue[index].receivers.insert(receiver);
-		else
-			queue.enqueue(entry);
-		locker.unlock();
-	}
-
-	void removeImageRequest(QString const & fileName, QObject * receiver)
-	{
-		QueueEntry entry;
-		entry.fileName = fileName;
-
-		QMutexLocker locker(&queueMutex);
-		int index = queue.indexOf(entry);
-		if (index >= 0)
-		{
-			queue[index].receivers.remove(receiver);
-			if (queue[index].receivers.isEmpty())
-				queue.removeAt(index);
-		}
-		locker.unlock();
-	}
+	void addImageRequest(QString const & fileName, QObject * receiver);
+	void removeImageRequest(QString const & fileName, QObject * receiver);
 
 protected:
-	void run() override
-	{
-		while (true)
-		{
-			QMutexLocker locker(&queueMutex);
-			if (queue.isEmpty())
-			{
-				locker.unlock();
-				msleep(100);
-				continue;
-			}
-
-			QueueEntry entry = queue.dequeue();
-			locker.unlock();
-
-			QPixmap p;
-			p.load(entry.fileName);
-			msleep(10);
-
-			for (QObject * receiver : entry.receivers)
-				emit imageLoaded(entry.fileName, receiver, p);
-			yieldCurrentThread();
-		}
-	}
+	void run() override;
 
 signals:
 	void imageLoaded(QString const & fileName, QObject const * receiver, QPixmap const & px);
